41aa538220fe013a150fade7526f208f663eafd5
Fatima A. <abdulw.fatima@gmail.com>
2024-01-09 15:29:40 +0100

    done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,38 @@
+import sort.Sorting;
+
+import java.util.List;
+import java.util.Scanner;
+
+import static sort.Sorting.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
         // TODO Initialise an int array with random numbers
+        Scanner input = new Scanner(System.in); // User gibt größer des Arrays an
+        System.out.println("Enter the array size: ");
+        int n = input.nextInt();
 
+        List<Integer> randomArray = generateRandomArray(n); //die random generierten Zahlen werden in die Liste gespeichert
+
+        //Liste wird in ein Array konvertiert
+        int[] arrayToSort = new int[randomArray.size()]; //neues Array wird erstellt mit der größe der Liste
+        for (int i = 0; i < randomArray.size(); i++) { //Schleife geht durch die Liste randomArray
+            arrayToSort[i] = randomArray.get(i); // Für jedes Element wird der Wert an der Position i aus randomArray genommen und in das entsprechende Element des Arrays arrayToSort geschrieben
+
+        }
 
         // TODO Sort the array with your sorting algorithm
-
+        Sorting sortingObj = new Sorting();
+        sortingObj.sort(arrayToSort, 0, arrayToSort.length - 1);
 
         // TODO Print the array to the console
+        System.out.println("Sorted Array:");
+        Sorting.printArray(arrayToSort);
 
-        // TODO Do a runtime analysis of your algorithm
-        //  For which array size does the execution time start to rise significantly?
+        System.out.println("Iteration counters: "+ countIteration);
 
 
-        // TODO Compare the execution time of your algorithm with the execution time of your QuickSort or BubbleSort implementation
-        
-
-        // TODO Print the iterations counters on the console
     }
 }
--- src/main/java/sort/Sorting.java
+++ src/main/java/sort/Sorting.java
@@ -1,24 +1,62 @@
 package sort;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Sorting {
-
     // TODO Write a method to generate an array of size N initialised with random numbers
+    public static List<Integer> generateRandomArray(int n) { // ArrayList erstellt, um User input interessanter zu gestalten
+        ArrayList<Integer> arr = new ArrayList<Integer>(n);
+        Random random = new Random();
 
-
+        for (int i = 0; i < n; i++) {
+            arr.add(random.nextInt(1000)); //Nachdem User die gewünschte größe des Arrays eingegeben hat, werden random Zahlen zwischen 0 und 1000
+        }
+        return arr;
+    }
     // TODO Implement a sorting algorithm of your choice
+
+    public static int countIteration;
+
+    int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            countIteration++;
+            if (arr[j] <= pivot) {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public void sort(int arr[], int low, int high) {
+        if (low < high) {
+            int index = partition(arr, low, high);
+            sort(arr, low, index - 1);
+            sort(arr, index + 1, high);
+        }
+    }
 
 
     // TODO Implement a printArray method
-
-
-    // TODO Write a method to do a runtime analysis of your algorithm.
-    //  For which array size does the execution time start to rise significantly?
-
-
-    // TODO Add global static counter variables to count the number of iterations for each algorithm
-
-
-    // TODO Compare the execution time of your algorithm with the execution time of your QuickSort or BubbleSort implementation
+    public static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
 
 
 }
